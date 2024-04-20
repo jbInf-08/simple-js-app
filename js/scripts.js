9afc378826774645bf34e29a6c526d36d46f98a1
Juan Bautista <144956427+jbInf-08@users.noreply.github.com>
2024-04-20 16:08:59 -0500

    Created a for loop displaying the items in the array with a message next to the largest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
+//Array list
 let pokemonList = [
-    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
-    {name: 'Pidgey', height: 1, types: ['flying', 'normal'] },
-    {name: 'Crobat', height: 5, types: ['flying', 'poison']}
+    { 
+        name: 'Bulbasaur', 
+        height: 7, 
+        types: ['grass', 'poison'] 
+    },
+    {
+        name: 'Pidgey', 
+        height: 1,
+        types: ['flying', 'normal'] 
+    },
+    {
+        name: 'Crobat',
+        height: 5,
+        types: ['flying', 'poison']
+    }
 ]
+//For loop
+for (let i=0; i<pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+    if (pokemonList[i].height > 5) {
+        document.write('- Whoa, that\'s huge!');
+    }
+    document.write('<br>');
+}
