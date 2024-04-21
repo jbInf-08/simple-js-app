c870fcbdb6de6d7ae5247225302cca85235ef5cf
Juan Bautista <144956427+jbInf-08@users.noreply.github.com>
2024-04-20 23:17:41 -0500

    created a foreach loop replacing the for loop wrapped array in IIFE and created a new repository used add, getall, and allowed IIFE to function and retrieve the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+let pokemonRepository = (function () {
 //Array list
 let pokemonList = [
     { 
@@ -15,12 +16,44 @@ let pokemonList = [
         height: 5,
         types: ['flying', 'poison']
     }
-]
+];
+function add(pokemon) {
+    if (typeof pokemon === 'object') {
+        pokemonList.push(pokemon);
+    }
+}
+function getAll() {
+    return pokemonList;
+
+}
+return {
+    add: add,
+    getAll: getAll
+}
+}());
+// add a new object(pokemon) to the pokemonList
+pokemonRepository.add({
+    name: 'Milotic',
+    height: 20,
+    types: ['water']
+});
 //For loop
-for (let i=0; i<pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
-    if (pokemonList[i].height > 5) {
+// for (let i=0; i<pokemonList.length; i++) {
+//   document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ');
+//    if (pokemonList[i].height > 5) {
+//        document.write('- Whoa, that\'s huge!');
+//    }
+//    document.write('<br>');
+//}
+//For each loop
+//pokemonList.forEach(function(pokemon) {
+//    document.write(pokemon.name);
+//});
+// print the list of names from pokemonList and coresponding height of that name
+pokemonRepository.getAll().forEach(function(pokemon) {
+    document.write(pokemon.name + ' (height: ' + pokemon.height + ') ');
+    if (pokemon.height > 5) {
         document.write('- Whoa, that\'s huge!');
     }
     document.write('<br>');
-}
+});
